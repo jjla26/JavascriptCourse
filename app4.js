a37572fc5a339976ba32e88d08610050ff632f57
Julio Lopez <julioj.lopeza@gmail.com>
2019-03-09 16:31:05 -0300

    return functions

--- app4.js
+++ app4.js
@@ -1,4 +1,6 @@
 
+// Parametros de las funciones
+
 function imprimir ( nombre, apellido ){
 
     apellido = apellido || "xxx"
@@ -29,3 +31,47 @@ imprimir( persona );
 imprimir2( function(){
     console.log("Funcion Anonima")
 });
+
+//retorno de las funciones
+
+
+function obtenerAleatorio(){
+    return Math.random();
+}
+
+function obtenerNombre(){
+    return "Juan"
+}
+
+function booleano(){
+    if( obtenerAleatorio() > 0.5 ){
+        return true; 
+    }else{
+        return false};
+    }
+console.log(obtenerAleatorio() + 10)
+
+console.log(obtenerNombre() + "Perez")
+
+if( booleano()){
+    console.log("es mayor a 0.5")
+}else{
+    console.log("es menor a 0.5")
+}
+
+function createFunction(){
+    return function(nombre){
+        console.log("Me Creo!!!"+ nombre )
+        return function(){
+            console.log("segunda function")
+        }
+    }
+}
+
+var nuevaFunction = createFunction();
+nuevaFunction(persona.nombre);
+
+var segundaFunction = nuevaFunction( persona.nombre );
+
+segundaFunction();
+
