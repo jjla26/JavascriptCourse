b86bbc621b433e097c2015505b1534a9d5aaa1a2
Julio Lopez <julioj.lopeza@gmail.com>
2019-03-09 14:29:06 -0300

    point notation

--- app3.js
+++ app3.js
@@ -1,3 +1,5 @@
+// valor y referencia
+
 var a = 10;
 var b = a; 
 
@@ -22,5 +24,44 @@ c.nombre = "Jose";
 
 console.log("c:",c);
 console.log("d:",d);
+
+//notacion de puntos y corchetes
+var persona = {
+    nombre: "Juana",
+    apellido: "Herrera",
+    edad: 25,
+    direccion: {
+        calle: "cienfuegos",
+        numero: 78,
+        edificio: {
+            numero: 303,
+            torre: "a"
+        }
+    }
+}
+
+//notacion de puntos
+console.log(persona.nombre)
+console.log(persona.direccion.calle)
+
+console.log(persona.direccion.edificio.numero)
+
+var edificio = persona.direccion.edificio;
+
+edificio.nopiso = "tercer piso";
+
+var campo = "nombre";
+
+console.log( persona[campo] )
+
+
+
+
+
+
+
+
+
+
 
 
