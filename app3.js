203f9e58aa6e433514d8936b46d3198a0d55a342
Julio Lopez <julioj.lopeza@gmail.com>
2019-03-09 13:36:11 -0300

    Value and Reference

---
+++ app3.js
@@ -0,0 +1,26 @@
+var a = 10;
+var b = a; 
+
+console.log("a:", a);
+console.log("b:", b);
+
+a = 20;
+
+console.log("a:", a);
+console.log("b:", b);
+
+var c = {
+    nombre: "Julio"
+};
+
+var d = c;
+
+console.log("c:",c);
+console.log("d:",d);
+
+c.nombre = "Jose";
+
+console.log("c:",c);
+console.log("d:",d);
+
+
