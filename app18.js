fafa231f79659ba5e54dd92ba306e86d6fd60567
Julio Lopez <julioj.lopeza@gmail.com>
2019-04-08 21:13:56 -0400

    Date Object

--- app18.js
+++ app18.js
@@ -1,42 +1,52 @@
-//numeric object
+// //numeric object
 
-var a = 10.1234567890;
-var b = new Number(10);
+// var a = 10.1234567890;
+// var b = new Number(10);
 
-console.log(a.toFixed(2));
+// console.log(a.toFixed(2));
 
-console.log(a.toPrecision(4))
+// console.log(a.toPrecision(4))
 
-a = a.toString(a);
-console.log(a);
+// a = a.toString(a);
+// console.log(a);
 
-//boolean object
+// //boolean object
 
-var c = true;
-var d = false;
+// var c = true;
+// var d = false;
 
-var e = new Boolean();
-console.log(e);
+// var e = new Boolean();
+// console.log(e);
 
-if (e){
-    console.log("hola");
-}
+// if (e){
+//     console.log("hola");
+// }
 
-//string object
+// //string object
 
-var k = new String("Julio");
-var l = "lopez"
-console.log( k );
+// var k = new String("Julio");
+// var l = "lopez"
+// console.log( k );
 
-console.log(k.toUpperCase());
-console.log(k.toLowerCase());
+// console.log(k.toUpperCase());
+// console.log(k.toLowerCase());
 
-var m = k.indexOf("i")
-var m = k.lastIndexOf("i")
-var nombre = k.substr(0,k.indexOf("i"));
-console.log(nombre)
-var k = "Julio Lopez"
-var split = k.split(" ");
-console.log(split);
+// var m = k.indexOf("i")
+// var m = k.lastIndexOf("i")
+// var nombre = k.substr(0,k.indexOf("i"));
+// console.log(nombre)
+// var k = "Julio Lopez"
+// var split = k.split(" ");
+// console.log(split);
+
+var hoy  = new Date();
+var fMili = new Date(0);
+var fFija = new Date(2016,9, 21, 23, 10,15,1);
 
 
+console.log(hoy);
+console.log(fMili);
+console.log(fFija);
+
+console.log(hoy.getFullYear()+1)
+console.log(hoy.getMonth())
