2404bf226101bd4c53823546ee33eb6f9aafda2d
Julio Lopez <julioj.lopeza@gmail.com>
2019-04-08 20:52:25 -0400

    String Object

--- app18.js
+++ app18.js
@@ -21,3 +21,22 @@ console.log(e);
 if (e){
     console.log("hola");
 }
+
+//string object
+
+var k = new String("Julio");
+var l = "lopez"
+console.log( k );
+
+console.log(k.toUpperCase());
+console.log(k.toLowerCase());
+
+var m = k.indexOf("i")
+var m = k.lastIndexOf("i")
+var nombre = k.substr(0,k.indexOf("i"));
+console.log(nombre)
+var k = "Julio Lopez"
+var split = k.split(" ");
+console.log(split);
+
+
