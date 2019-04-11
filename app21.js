c45080663cf3382a3b6ff91b87ec1543dcf828bd
Julio Lopez <julioj.lopeza@gmail.com>
2019-04-10 22:32:41 -0400

    ';' and comments

---
+++ app21.js
@@ -0,0 +1,34 @@
+function getNumber(){
+    return ; 10
+
+}
+console.log(getNumber()) // the ";" is necesary because weird things could happen
+
+/*
+adasd
+asd
+asd
+asd
+asd
+*/
+
+var arr = ["Fernando",{
+  //La primera posicio...
+  //sirve ...
+  //hace ..
+    nombre:"Fernando",
+
+    //Asdlkñsa
+    //ñaskd
+    apellido:"Herrera",
+    getNombre:function(){
+        return this.nombre+" "+ this.apellido
+    }
+}, true, function(){ console.log(this);}];
+
+
+
+//asñdlkañlsd
+ñlaskdñkasd
+
+console.log(arr)
