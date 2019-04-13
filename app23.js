f061f594af15bdba693b22a54db498761111133c
Julio Lopez <julioj.lopeza@gmail.com>
2019-04-13 19:48:41 -0400

    cookies

--- app23.js
+++ app23.js
@@ -1,11 +1,31 @@
 
 function crearCookie(nombre, valor){
-    valor = escape(valor);
+    valor = escape(valor)
     var hoy = new Date();
-    console.log(hoy)
     hoy.setMonth( hoy.getMonth() + 1);
-    console.log(hoy)
     document.cookie = nombre+"="+valor+";expires="+hoy.toUTCString()+";";
+}
+
+function borrarCookie( nombre ){
+    var hoy = new Date();
+    hoy.setMonth( hoy.getMonth() - 1);
+    document.cookie = nombre+"=x;expires="+hoy.toUTCString()+";";
+}
+
+function getCookie( nombre ){
+    var cookies = document.cookie;
+    var cookieArr = cookies.split("; ");
+    console.log(cookieArr);
+
+    for( var i=0; i < cookieArr.length; i++){
+        var parArr = cookieArr[i].split("=")
+        cookieArr[i]= parArr;
+        if(parArr[0] === nombre){
+            return unescape(parArr[1])
+        }
+    }
+    console.log(cookieArr)
+    return undefined;
 }
 
 // function borrarCookie(nombre){
@@ -13,6 +33,11 @@ function crearCookie(nombre, valor){
 // }
 
 crearCookie("nombre","Fernando");
+crearCookie("apellido","Lopez");
 
+// borrarCookie("nombre");
+console.log(getCookie("apellido"));
 //  var cookies = document.cookie;
 //  console.log(cookies);
+
+
