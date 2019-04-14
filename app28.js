0bdcf5b83b5d7d0efd94ffb5c780d6d6865e8116
Julio Lopez <julioj.lopeza@gmail.com>
2019-04-13 21:58:38 -0400

    while and do while

---
+++ app28.js
@@ -0,0 +1,20 @@
+var i = 1;
+
+while( i<=10 ){
+    i++;
+    if (i===4){
+        continue;
+    }
+    if (i===5){
+        break;
+    }
+
+    console.log(i);
+}
+
+do{
+    i++
+    console.log(i)
+
+
+}while( i<10);
