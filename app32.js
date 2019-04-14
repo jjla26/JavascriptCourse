88cbb68bd3b4f51160623f93c359f097e3cba0e3
Julio Lopez <julioj.lopeza@gmail.com>
2019-04-14 00:08:15 -0400

    Events

---
+++ app32.js
@@ -0,0 +1,10 @@
+//Events
+
+function presionar(arg){
+    console.log("asd");
+    console.log(arg);
+}
+
+var objeto = document.getElementById("texto")
+objeto.addEventListener("click",presionar)
+console.log(objeto)
