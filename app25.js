4bb5ac1800bf6e61886062402889a7adf3111a6b
Julio Lopez <julioj.lopeza@gmail.com>
2019-04-13 21:33:31 -0400

    Switch statement

--- app25.js
+++ app25.js
@@ -1,28 +1,50 @@
-var nota = 60;
-var nota2= 80;
-var d = undefined;
-if( nota >80){
-    console.log("Excelente")
-}else if ( nota > 60){
-    console.log("Aprobo")
-}else if ( nota > 30){
-    console.log("Mal")
+// var nota = 60;
+// var nota2= 80;
+// var d = undefined;
+// if( nota >80){
+//     console.log("Excelente")
+// }else if ( nota > 60){
+//     console.log("Aprobo")
+// }else if ( nota > 30){
+//     console.log("Mal")
 
+// }
+
+
+// else{
+//     console.log("Reprobo")
+// }
+
+// var c = (nota > nota2)? function(){
+//     console.log( nota, "es mayor que:", nota2)
+// }() : function(){
+//     console.log( nota2, "es mayor que:", nota)
+// }();
+
+// console.log(c)
+
+// var e = d || nota;
+
+// console.log(e)
+
+//Switch
+
+var mes = 2;
+
+switch (mes) {
+    case (10 > 40)? 10 : 40 :
+        console.log("Enero");
+        break;    
+    case 2:
+        console.log("Febrero");
+        break;
+    case 3:
+        console.log("Marzo");
+        break;
+    default:
+        console.log("Cualquier otro mes");
+        break;
+
+
+    
 }
-
-
-else{
-    console.log("Reprobo")
-}
-
-var c = (nota > nota2)? function(){
-    console.log( nota, "es mayor que:", nota2)
-}() : function(){
-    console.log( nota2, "es mayor que:", nota)
-}();
-
-console.log(c)
-
-var e = d || nota;
-
-console.log(e)
