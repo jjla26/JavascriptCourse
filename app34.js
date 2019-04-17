b7f5426c2332a01aaf33b55d71c5e751718ae94c
Julio Lopez <julioj.lopeza@gmail.com>
2019-04-16 21:56:28 -0400

    dialog boxes

---
+++ app34.js
@@ -0,0 +1,18 @@
+"use strict";
+//dialog boxes
+
+
+alert("hola")
+
+var acepto = confirm("Esta seguro de que desea borrar algo ?")
+
+console.log(acepto)
+
+var nombre = prompt("ingrese su nombre","nombre")
+console.log(nombre)
+
+//Use strict
+
+nombre1 = "Julio";
+
+console.log(nombre1)
